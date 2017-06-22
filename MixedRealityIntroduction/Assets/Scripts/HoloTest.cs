d00ce20244169baafe1e66bd304ca994ee3450a3
Monroe <monroe@61bm.com>
2017-06-22 17:23:48 +0800

    frist push

---
+++ MixedRealityIntroduction/Assets/Scripts/HoloTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = new
+            Color(Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f,
+            Random.Range(0, 255) / 255f);
+    }
+}
